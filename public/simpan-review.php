c2b8635e378ffae7ea03ab46353c171c23c57f10
jwreeikh <rhehanarizky@gmail.com>
2026-04-01 22:16:31 +0700

    Add comment for clarity in review insertion
    Added a comment to clarify the purpose of the code.

--- public/simpan-review.php
+++ public/simpan-review.php
@@ -5,6 +5,7 @@ $tamu_id = $_POST['tamu_id'];
 $rating = $_POST['rating'];
 $tags = $_POST['tags'];
 
+//menyimpan data review ke dalam tabel review di database
 mysqli_query($conn, 
 "INSERT INTO review (tamu_id, rating, tags) 
 VALUES ('$tamu_id','$rating','$tags')");
