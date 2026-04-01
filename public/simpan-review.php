04dbbcefed1700b419ef54bccb1c608c2fd23c01
jwreeikh <rhehanarizky@gmail.com>
2026-04-01 22:19:39 +0700

    Add comments for clarity in simpan-review.php

--- public/simpan-review.php
+++ public/simpan-review.php
@@ -1,9 +1,9 @@
 <?php
 include '../config/koneksi.php';
 
-$tamu_id = $_POST['tamu_id']; //id tamu
-$rating = $_POST['rating'];
-$tags = $_POST['tags'];
+$tamu_id = $_POST['tamu_id']; //id tamu (fk dari tabel tamu)
+$rating = $_POST['rating']; //nilai rating
+$tags = $_POST['tags']; //tag review
 
 //menyimpan data review ke dalam tabel review di database
 mysqli_query($conn, 
